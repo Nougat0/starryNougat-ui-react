4ef79e2425f93831c3638bcdab984c5a1c465eb3
hmkim <elppapie@naver.com>
2023-08-31 00:08:36 +0900

    calendarPage 헤더 레이아웃 구성
    header의 기초 구조 잡기 위한 css 설정 진행됨

--- src/components/pages/CalendarPage.js
+++ src/components/pages/CalendarPage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./CalendarPage.css";
 
 const CalendarPage = () => {
-    return <>캘린더 페이지입니다</>;
+    return (
+        <div>
+            <div className="calendar-title">캘린더 페이지입니다</div>
+            <div className="calendar">
+                <div className="calendar-header">
+                    <div className="calendar-yymm">2023.08</div>
+                    <div className="calendar-updateInfo">
+                        <div className="calendar-author">
+                            네이버카페_포켓몬GO한국_별밤 편집
+                        </div>
+                        <div className="calendar-updateDate">
+                            2023.08.30 업데이트
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CalendarPage;
