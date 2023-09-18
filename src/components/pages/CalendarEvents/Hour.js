355d149e2e7c0179c1dc1ab4675a81671d348794
hmkim <elppapie@naver.com>
2023-09-18 09:41:15 +0900

    Hour 이벤트 구성 (진행중)
    아워의 div를 어떻게 구성할 것인지 한번 청사진을 짜 놓고 다시 구성해야 할 듯.
    아워 pm과 아워 info 가 각각 양 끝단에 배치되대, 아워 pm은 우측 끝에 걸치듯이 있었으면 좋겠음 (선을 넘어도 되게끔) 그걸 위한 grid 설계가 추가로 필요함

---
+++ src/components/pages/CalendarEvents/Hour.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hour = () => {
+    return <div className="day-event"></div>;
+};
+
+export default Hour;
--- src/components/pages/CalendarPage/CalendarDay.js
+++ src/components/pages/CalendarPage/CalendarDay.js
@@ -3,7 +3,8 @@ import React from "react";
 /**
  *
  * @param {Date} day 표시하는 날짜의 full string
- * @param {String} customClass 속성 주기 위해서 붙일 클래스
+ * @param {String} customFontClass 폰트 속성 위해서 붙일 클래스
+ * @param {String} customDayClass  위해서 붙일 클래스
  * @returns
  */
 const CalendarDay = ({ day, customFontClass, customDayClass }) => {
@@ -29,7 +30,31 @@ const CalendarDay = ({ day, customFontClass, customDayClass }) => {
                 </div>
                 <div className="day-event">이벤트영역</div>
             </div>
-            <div className="day-body"></div>
+            <div className="day-body">
+                {day.getDate() === 8 && (
+                    <div className="hour-container">
+                        <div className="hour-header">
+                            <div className="hour-header-info">
+                                <div className="hour-header-text">
+                                    <div className="hour-bonus">
+                                        아워 보너스
+                                    </div>
+                                    <div className="hour-time">아워 시각</div>
+                                </div>
+                                <div className="hour-pm">
+                                    <img
+                                        src="/pm/pm200(1).png"
+                                        alt="아워 포켓몬"
+                                    ></img>
+                                </div>
+                            </div>
+
+                            <div className="hour-title">아워 이벤트</div>
+                        </div>
+                        <div className="hour-body"></div>
+                    </div>
+                )}
+            </div>
             {/* 레이드 정보는 달력 위에 float로 위치 계산해서 놓는걸로 */}
         </div>
     );
