c6611c0229b07c462fc82f37a1c078ed19bf9941
hmkim <elppapie@naver.com>
2024-11-07 23:59:35 +0900

    :package: 달력에 필요한 아이콘 이미지 파일 추가
    Icons.js 파일로 동적으로 아이콘을 가져올 수 있도록 Map 구성, alt 도 포함
    alt 도 같이 넣어서 사용

---
+++ src/components/calendar/Icons.js
@@ -0,0 +1,23 @@
+let Icons = {
+    EGG: {src:`/resource/icons/egg.png`, alt: `알 부화`},
+    EVOLUTION: {src:`/resource/icons/evolution.png`, alt: `야생 등장`},
+    FIELD_RESEARCH: {src:`/resource/icons/field_research.png`, alt: `필드 리서치`},
+    WILD: {src:`/resource/icons/wild.png`, alt: `야생 등장`},
+    SHOWCASE: {src:`/resource/icons/showcase.png`, alt: `쇼케이스`},
+    RAID: {src:`/resource/icons/raid.png`, alt: `레이드 등장`},
+    SHINY: {src:`/resource/icons/shiny.png`, alt: `이로치 등장`},
+    CANDY: {src:`/resource/icons/Candy.png`, alt: `사탕`},
+    CANDYXL: {src:`/resource/icons/CandyXL.png`, alt: `사탕XL`},
+    PARTNER: {src:`/resource/icons/partner.png`, alt: `파트너`},
+    PURIFIED: {src:`/resource/icons/purified.png`, alt: `정화`},
+    EVOLUTION: {src:`/resource/icons/evolution.png`, alt: `진화`},
+    EXCHANGE: {src:`/resource/icons/exchange.png`, alt: `교환`},
+    ROCKET: {src:`/resource/icons/go_rocket.png`, alt: `로켓단`},
+    LG_RAID: {src:`/resource/icons/legend_raid.png`, alt: `전설 레이드`},
+    ME_RAID: {src:`/resource/icons/mega_raid.png`, alt: `메가 레이드`},
+    ROUTE: {src:`/resource/icons/route.png`, alt: `루트`},
+    SHADOW: {src:`/resource/icons/shadow.png`, alt: `그림자`},
+
+}
+
+export default Icons;
